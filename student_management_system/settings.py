a9163e440a87b2a41f7a5609c479fc12203c1c8b
Akash Shrivastava <akash.shrivastava136@gmail.com>
2023-03-27 13:31:00 +0530

    AS | Update staticfiles storage class

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -146,7 +146,7 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_mails")
 #
 
 # Enable Only Making Project Live on Heroku
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # import dj_database_url
 # prod_db=dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
